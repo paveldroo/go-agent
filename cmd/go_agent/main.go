5551b751e91444b9a1c0e9864f7f3f743c4dc672
Pavel Druzhinin <paveldroo@gmail.com>
2026-08-26 01:29:04 +0300

    add cli args parsing

--- cmd/go_agent/main.go
+++ cmd/go_agent/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var task string
+	flag.StringVar(&task, "task", "", "task for agent")
+
+	flag.Parse()
+
+	if len(task) == 0 {
+		fmt.Fprint(os.Stderr, "you should define a task for agent\n")
+		os.Exit(1)
+	}
+
+	fmt.Println(task)
+}
